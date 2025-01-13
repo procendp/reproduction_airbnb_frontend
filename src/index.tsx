cec28d53b8286ea1ebf376adee497accad51a270
procendp <procendp@gmail.com>
2025-01-13 23:06:53 +0900

    applied react-query

--- src/index.tsx
+++ src/index.tsx
@@ -7,19 +7,23 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import theme from "./theme";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const client = new QueryClient();
 const container = document.getElementById("root");
 if (!container) throw new Error("Failed to find the root element");
 const root = ReactDOM.createRoot(container);
 
 root.render(
   <React.StrictMode>
-    {/* <ColorModeScript /> */}
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      {/* 사용자가 이전에 어떤 모드 사용했는지 확인 */}
-      <RouterProvider router={router} />
-    </ChakraProvider>
+    <QueryClientProvider client={client}>
+      {/* <ColorModeScript /> */}
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        {/* 사용자가 이전에 어떤 모드 사용했는지 확인 */}
+        <RouterProvider router={router} />
+      </ChakraProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
