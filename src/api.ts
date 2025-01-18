9c9cf023cf5d1f29bf78a204964734f8b9c08043
procendp <procendp@gmail.com>
2025-01-18 22:28:01 +0900

    making login system

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,6 @@
 import { QueryFunctionContext } from "@tanstack/react-query";
 import axios from "axios";
+import { resolve } from "dns";
 
 const instance = axios.create({
   baseURL: "http://127.0.0.1:8000/api/v1",
@@ -19,3 +20,6 @@ export const getRoomReviews = ({ queryKey }: QueryFunctionContext) => {
     .get(`rooms/${roomPk}/reviews`)
     .then((response) => response.data);
 };
+
+export const getMe = () =>
+  instance.get("users/me").then((response) => response.data);
