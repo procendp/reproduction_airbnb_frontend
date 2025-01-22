54010e4a8280900aafa29c56586ae43605e4cbf5
procendp <procendp@gmail.com>
2025-01-22 22:28:38 +0900

    made immediately log-out function and solve the CSRF error

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,6 +20,8 @@ import { Link } from "react-router-dom";
 import LoginModal from "./LoginModal";
 import SignUpModal from "./SignUpModal";
 import useUser from "../lib/useUser";
+import { logOut } from "../api";
+import { useQueryClient } from "@tanstack/react-query";
 
 export default function Header() {
   const { userLoading, isLoggedIn, user } = useUser();
@@ -37,6 +39,7 @@ export default function Header() {
   const logoColor = useColorModeValue("red.500", "red.200");
   const Icon = useColorModeValue(FaMoon, FaSun);
   const toast = useToast();
+  const queryClient = useQueryClient();
   const onLogOut = async () => {
     const toastId = toast({
       title: "Login out...",
@@ -44,15 +47,13 @@ export default function Header() {
       status: "loading",
       position: "bottom-right",
     });
-    // const data = await logOut();
-    // console.log(data);
-    setTimeout(() => {
-      toast.update(toastId, {
-        status: "success",
-        title: "Done!",
-        description: "See you later!",
-      });
-    }, 5000); // 5000ms -> 5s
+    await logOut();
+    queryClient.refetchQueries(["me"]);
+    toast.update(toastId, {
+      status: "success",
+      title: "Done!",
+      description: "See you later!",
+    });
   };
   return (
     <Stack
