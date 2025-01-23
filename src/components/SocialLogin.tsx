10adf29bc99449f027fcd5674174bcdc8d870b2c
procendp <procendp@gmail.com>
2025-01-23 23:58:16 +0900

    made log-in system with github account

--- src/components/SocialLogin.tsx
+++ src/components/SocialLogin.tsx
@@ -21,7 +21,12 @@ export default function SocialLogin() {
       </HStack>
       <VStack>
         <LightMode>
-          <Button w="100%" leftIcon={<FaGithub />} colorScheme={"telegram"}>
+          <Button
+            as="a"
+            href="https://github.com/login/oauth/authorize?client_id=Ov23liPfh3H8KNxVkYCb&scope=read:user,user:email"
+            w="100%"
+            leftIcon={<FaGithub />}
+          >
             Continue with Github
           </Button>
           <Button w="100%" leftIcon={<FaComment />} colorScheme={"yellow"}>
