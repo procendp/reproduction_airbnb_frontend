a0ecd6078bef498f7623c178c6ba238f6381fd1c
procendp <procendp@gmail.com>
2025-02-02 00:45:28 +0900

    made the uploading system to cloudfare

--- src/routes/UploadPhotos.tsx
+++ src/routes/UploadPhotos.tsx
@@ -7,14 +7,42 @@ import {
   Input,
   VStack,
 } from "@chakra-ui/react";
+import { useMutation } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
 import { useParams } from "react-router-dom";
+import { getUploadURL, uploadImage } from "../api";
 import useHostOnlyPage from "../components/HostOnlyPage";
 import ProtectedPage from "../components/ProtectedPage";
+
+interface IForm {
+  file: FileList;
+}
+
+interface IUploadURLResponse {
+  id: string;
+  uploadURL: string;
+}
+
 export default function UploadPhotos() {
-  const { register, watch } = useForm();
+  const { register, handleSubmit, watch } = useForm<IForm>();
   const { roomPk } = useParams();
+  const uploadImageMutation = useMutation(uploadImage, {
+    onSuccess: (data: any) => {
+      console.log(data);
+    },
+  });
+  const uploadURLMutation = useMutation(getUploadURL, {
+    onSuccess: (data: IUploadURLResponse) => {
+      uploadImageMutation.mutate({
+        uploadURL: data.uploadURL,
+        file: watch("file"),
+      });
+    },
+  });
   useHostOnlyPage();
+  const onSubmit = (data: any) => {
+    uploadURLMutation.mutate();
+  };
   return (
     <ProtectedPage>
       <Box
@@ -27,11 +55,16 @@ export default function UploadPhotos() {
       >
         <Container>
           <Heading textAlign={"center"}>Upload a Photo</Heading>
-          <VStack spacing={5} mt={10}>
+          <VStack
+            as="form"
+            onSubmit={handleSubmit(onSubmit)}
+            spacing={5}
+            mt={10}
+          >
             <FormControl>
               <Input {...register("file")} type="file" accept="image/*" />
             </FormControl>
-            <Button w="full" colorScheme={"red"}>
+            <Button type="submit" w="full" colorScheme={"red"}>
               Upload photos
             </Button>
           </VStack>
