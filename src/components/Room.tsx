c4779a01acb92cc1083bfbd8e449ca8832e89f15
procendp <procendp@gmail.com>
2025-02-02 00:03:20 +0900

    making upload images form

--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -1,4 +1,4 @@
-import { FaRegHeart, FaStar } from "react-icons/fa";
+import { FaCamera, FaRegHeart, FaStar } from "react-icons/fa";
 import {
   Box,
   Button,
@@ -9,7 +9,8 @@ import {
   useColorModeValue,
   VStack,
 } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import React from "react";
 
 interface IRoomProps {
   imageUrl: string;
@@ -19,6 +20,7 @@ interface IRoomProps {
   country: string;
   price: number;
   pk: number;
+  isOwner: boolean;
 }
 export default function Room({
   pk,
@@ -28,8 +30,14 @@ export default function Room({
   city,
   country,
   price,
+  isOwner,
 }: IRoomProps) {
   const gray = useColorModeValue("gray.500", "gray.300");
+  const navigate = useNavigate();
+  const onCameraClick = (event: React.SyntheticEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    navigate(`/rooms/${pk}/photos`);
+  };
   return (
     <Link to={`/rooms/${pk}`}>
       <VStack spacing={-0.5} alignItems={"flex-start"}>
@@ -57,9 +65,10 @@ export default function Room({
             position="absolute"
             top={0}
             right={0}
+            onClick={onCameraClick}
             color="white"
           >
-            <FaRegHeart size="20px" />
+            {isOwner ? <FaCamera size="20px" /> : <FaRegHeart size="20px" />}
           </Button>
         </Box>
         <Grid gap={5} templateColumns={"6fr 1fr"}>
