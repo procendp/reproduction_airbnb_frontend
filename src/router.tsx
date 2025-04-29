5000334afb910bd9710e0918c9050d4b100f13a5
procendp <procendp@gmail.com>
2025-04-30 02:28:17 +0900

    fix: remove basename from router config

--- src/router.tsx
+++ src/router.tsx
@@ -9,47 +9,42 @@ import RoomDetail from "./routes/RoomDetail";
 import UploadPhotos from "./routes/UploadPhotos";
 import UploadRoom from "./routes/UploadRoom";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <Root />,
-      errorElement: <NotFound />,
-      children: [
-        {
-          path: "",
-          element: <Home />,
-        },
-        {
-          path: "rooms/upload",
-          element: <UploadRoom />,
-        },
-        {
-          path: "rooms/:roomPk",
-          element: <RoomDetail />,
-        },
-        // {
-        //   path: "rooms/:roomPk/edit",
-        //   element: <EditRoom />,
-        // },
-        {
-          path: "rooms/:roomPk/photos",
-          element: <UploadPhotos />,
-        },
-      ],
-    },
-    {
-      path: "/social/github",
-      element: <GithubConfirm />,
-    },
-    {
-      path: "/social/kakao",
-      element: <KakaoConfirm />,
-    },
-  ],
+const router = createBrowserRouter([
   {
-    basename: process.env.PUBLIC_URL,
-  }
-);
+    path: "/",
+    element: <Root />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "rooms/upload",
+        element: <UploadRoom />,
+      },
+      {
+        path: "rooms/:roomPk",
+        element: <RoomDetail />,
+      },
+      // {
+      //   path: "rooms/:roomPk/edit",
+      //   element: <EditRoom />,
+      // },
+      {
+        path: "rooms/:roomPk/photos",
+        element: <UploadPhotos />,
+      },
+    ],
+  },
+  {
+    path: "/social/github",
+    element: <GithubConfirm />,
+  },
+  {
+    path: "/social/kakao",
+    element: <KakaoConfirm />,
+  },
+]);
 
 export default router;
