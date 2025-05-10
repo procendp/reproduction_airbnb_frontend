5efaeb306cd48c6f8ea582a6e8f60de4fdde604b
procendp <procendp@gmail.com>
2025-05-11 01:49:35 +0900

    fix: prevent unnecessary user info requests when not logged in (enabled option in useUser)

--- src/lib/useUser.ts
+++ src/lib/useUser.ts
@@ -1,14 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import { getMe } from "../api";
+import Cookie from "js-cookie";
 
 export default function useUser() {
+  const hasSession = Boolean(Cookie.get("sessionid"));
   const { isLoading, data, isError } = useQuery(["me"], getMe, {
     retry: 2,
     cacheTime: 0,
+    enabled: hasSession,
   });
   return {
     userLoading: isLoading,
     user: data,
-    isLoggedIn: !isError,
+    isLoggedIn: !isError && !!data,
   };
 }
